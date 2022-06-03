dba0a73c85640ab4e74d3946effb994a2e2df4df
Felipe Gonzalez <felipegonzalezurr@gmail.com>
2022-06-02 22:37:25 -0400

    Resterver MVC Node

---
+++ models/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors');
+class Server{
+
+constructor(){
+    this.app = express();
+    this.port= process.env.PORT;
+    this.usuariosPath = '/api/usuarios';
+     //middlewares
+     this.middlewares();
+     //rutas de mi aplicacion
+
+    this.routes();
+}
+middlewares(){
+    //directorio publico
+    this.app.use(cors());
+    this.app.use(express.static('public'));
+}
+  
+routes() {
+  
+    this.app.use( this.usuariosPath, require('../routes/user'));
+}
+listen() {
+    this.app.listen(this.port,() =>{
+        console.log('Servidor corriendo en puerto', process.env.PORT)
+    });
+}
+}
+
+
+module.exports = Server
