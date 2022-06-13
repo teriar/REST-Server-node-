5dcf2dc7b5849921e752d99920781d1d51b3da8c
Felipe Gonzalez <felipegonzalezurr@gmail.com>
2022-06-13 18:22:03 -0400

    agregado google signInt contraseña a evaluar cambios

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const login = require('../controllers/auth.controller.js');
+const {login, googleSingIn} = require('../controllers/auth.controller.js');
 const { validarCampos } = require('../middlewares/validar-campos.js');
 
 
@@ -12,6 +12,11 @@ router.post('/login',[
     validarCampos
 ], login)
 
+router.post('/google',[
+    check('id_token', 'id_token es necesario').not().isEmpty(),
+    
+    validarCampos
+], googleSingIn)
 
 
 
