128bfdcae248368b5250c97eb4dbefee077270c6
Felipe Gonzalez <felipegonzalezurr@gmail.com>
2022-06-21 21:24:36 -0400

    Anadido carpeta assets y no-image

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -18,20 +18,13 @@ const cargarArchivo = async(req,res = response)=>{
     res.status(400).json({err, dsa:'sa'
   
     });
-   }
-   
-
-    
-    
+   } 
 
 }
 
 const ActualizarImagen= async(req,res=response)=>{
-   
-
 
   
-
     const{id,coleccion}= req.params;
    
    let modelo;
@@ -90,4 +83,68 @@ const ActualizarImagen= async(req,res=response)=>{
 
 }
 
-module.exports ={ cargarArchivo,ActualizarImagen}
+
+
+const mostrarImagen =async(req,res=response)=>{
+
+  const{id,coleccion}= req.params;
+   
+  let modelo;
+ 
+  switch (coleccion) {
+   case 'usuarios':
+      modelo = await Usuario.findById(id);
+      if(!modelo){
+       return res.status(400).json({
+         msg: `No existe usuario con este ID: ${id}`
+       })
+      }
+     break;
+    
+     case 'productos':
+       modelo = await Producto.findById(id);
+       
+       if(!modelo){
+        return res.status(400).json({
+          msg: `No existe producto con este ID: ${id}`
+        })
+       }
+     break;
+  
+   default:
+     return res.status(500).json({
+       msg:`Error al actualizar imagen en, este errror puede ocurrir 
+       si las hubo un error en las rutas estipuladas: ${coleccion} y ${id} 
+       comuniquese con administrador `
+     })
+     break;
+
+  }
+  //limpiar imagenes 
+  try{
+
+       if(modelo.img){
+        
+           const pathImagen = path.join(__dirname, '../uploads',coleccion, modelo.img); //el path
+           if(fs.existsSync(pathImagen)){ //si existe
+            return  res.sendFile(pathImagen)
+           }
+       }
+
+  }catch(err){
+   console.log(err);
+  }
+
+  
+        const pathImagen = path.join(__dirname, '../assets','no-image.jpg')
+        if(fs.existsSync(pathImagen)){ //si existe
+          return  res.sendFile(pathImagen)
+         }
+  
+
+}
+module.exports ={ 
+  cargarArchivo,
+  ActualizarImagen,
+  mostrarImagen
+}
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { cargarArchivo, ActualizarImagen } = require('../controllers/uploads.controller.js');
+const { cargarArchivo, ActualizarImagen, mostrarImagen } = require('../controllers/uploads.controller.js');
 const { coleccionesPermitidas } = require('../helpers');
 const { validarArchivo,validarCampos } = require('../middlewares');
 const {  } = require('../middlewares/validar-campos.js');
@@ -11,7 +11,12 @@ const {  } = require('../middlewares/validar-campos.js');
 const router = Router();
 
 
-router.post('/',validarArchivo,cargarArchivo);
+router.post('/',[
+    validarArchivo,
+    check('id','ID no es de Mongo').isMongoId(),
+    check('coleccion').custom(c=> coleccionesPermitidas(c,['usuarios','productos'])),
+    validarCampos
+],validarArchivo,cargarArchivo);
 
 router.put('/:coleccion/:id',[
     validarArchivo,
@@ -19,5 +24,13 @@ router.put('/:coleccion/:id',[
     check('coleccion').custom(c=> coleccionesPermitidas(c,['usuarios','productos'])),
     validarCampos
 ],ActualizarImagen)
+
+
+router.get('/:coleccion/:id',[
+    
+    check('id','ID no es de Mongo').isMongoId(),
+    check('coleccion').custom(c=> coleccionesPermitidas(c,['usuarios','productos'])),
+    validarCampos
+], mostrarImagen)
 
 module.exports = router;
